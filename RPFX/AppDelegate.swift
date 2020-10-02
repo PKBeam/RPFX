20a2113b3f6f80b238939004a4e08187260bef78
Vincent Liu <pkchu99@gmail.com>
2020-10-02 19:05:24 +1000

    update to new SwordRPC, get RPFX working for macOS 11

--- RPFX/AppDelegate.swift
+++ RPFX/AppDelegate.swift
@@ -144,3 +144,9 @@ extension AppDelegate: SwordRPCDelegate {
     
     }
 }
+
+struct AppDelegate_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
